b0014635ea30dae3807d80bacb8ee940a31afd68
watanave <susan.se3p@gmail.com>
2020-03-06 13:30:11 +0900

    workerページのリンクを修正

--- pages/worker.vue
+++ pages/worker.vue
@@ -18,19 +18,14 @@ export default {
     return {
       items: [
         {
-          title: '中小企業者等特別相談窓口',
+          title: '中小企業向け相談窓口及び融資取扱について',
           body:
-            '<a href="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/01/30/15.html" target="_blank" rel="noopener">https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/01/30/15.html</a> <br />資金繰りに関する相談、経営に関する相談（東京都産業労働局　報道発表）'
+            '<a href="http://www.pref.hokkaido.lg.jp/kz/csk/kny/yuushi/korona.htm" target="_blank" rel="noopener">http://www.pref.hokkaido.lg.jp/kz/csk/kny/yuushi/korona.htm</a> <br />新型コロナウイルス関連肺炎の流行に伴う中小企業向け相談窓口及び融資取扱について'
         },
         {
-          title: '緊急労働相談ダイヤル',
+          title: '新型コロナ感染症の影響による特別労働相談窓口',
           body:
-            '<a href="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/02/26/22.html" target="_blank" rel="noopener">https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/02/26/22.html</a><br />新型コロナウイルスに関する休暇や休業の取り扱い、職場のハラスメントなどについての相談（東京都産業労働局　報道発表）'
-        },
-        {
-          title: '新しいワークスタイルや企業活動の東京モデル「スムーズビズ」',
-          body:
-            '<a href="https://smooth-biz.metro.tokyo.lg.jp/" target="_blank" rel="noopener">https://smooth-biz.metro.tokyo.lg.jp/</a><br />テレワーク・時差出勤などスムーズビズの取組は、新型コロナウイルス感染症の対策としても効果的です。感染症対策として、東京2020大会時の交通混雑緩和に向けた取組の前倒しをお願いします。'
+            '<a href="http://www.pref.hokkaido.lg.jp/kz/rkr/korona.htm" target="_blank" rel="noopener">http://www.pref.hokkaido.lg.jp/kz/rkr/korona.htm</a><br />今般の新型コロナウイルス感染症により影響を受ける事業主、労働者の方向けの、相談窓口や各種支援措置をお知らせします。'
         }
       ]
     }
